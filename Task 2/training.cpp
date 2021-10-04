8b0de3fbad7af99de52671c835c0e31f50d26159
ILYA STRUK <43352343+ILYASTRUK@users.noreply.github.com>
2021-10-04 18:32:45 +0300

    Create training.cpp

---
+++ Task 2/training.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctype.h>
+#include <string>
+#include <cstring>
+#include "String.h"
+using namespace std;
+
+int main()
+{
+    String one;
+    one.getData();
+    one.swapsymbol();
+    one.show();
+    return  0;
+}
